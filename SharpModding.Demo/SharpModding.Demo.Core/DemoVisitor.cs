1f2c7e73616e2d848a686c8a00c871e64957c00a
Kevin Haumer <haumer.k00@htl-ottakring.ac.at>
2020-11-12 16:30:25 +0100

    Added DemoVisitor Core
    Co-Authored-By: Leon <46990304+mordjunior@users.noreply.github.com>

---
+++ SharpModding.Demo/SharpModding.Demo.Core/DemoVisitor.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System;
+using System.ComponentModel;
+using System.Net.Sockets;
+using System.Text;
+
+namespace SharpModding.Demo.Core
+{
+    public class DemoVisitor : CSharpSyntaxWalker
+    {
+        private JavaFileGenerator generator;
+
+        public DemoVisitor(string path) : base()
+        {
+            generator = new JavaFileGenerator(path);
+        }
+
+        public override void VisitUsingDirective(UsingDirectiveSyntax node)
+        {
+            generator.AddLine($"import {node.Name};", false);
+            base.VisitUsingDirective(node);
+        }
+
+        public override void VisitClassDeclaration(ClassDeclarationSyntax node)
+        {
+            StringBuilder line = new StringBuilder();
+
+            foreach (var item in node.Modifiers)
+            {
+                line.Append($"{item} ");
+            }
+            line.Append($"class {node.Identifier.ValueText}");
+
+            if (node.BaseList != null)
+            {
+                line.Append(" : ");
+
+                for (int i = 0; i < node.BaseList.Types.Count; i++)
+                {
+                    var item = node.BaseList.Types[i];
+                    line.Append(item.Type);
+                    if (i != node.BaseList.Types.Count - 1)
+                        line.Append(", ");
+                }
+
+            }
+            generator.AddLine(line.ToString(), true);
+
+            base.VisitClassDeclaration(node);
+        }
+    }
+}
---
+++ SharpModding.Demo/SharpModding.Demo.Core/JavaFileGenerator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharpModding.Demo.Core
+{
+    class JavaFileGenerator
+    {
+        private string _path;
+        private List<string> lines = new List<string>();
+
+        public JavaFileGenerator(string path)
+        {
+            _path = path;
+        }
+
+        public void AddLine(string line, bool addCurvedBrackets)
+        {
+            lines.Add(line);
+            if (addCurvedBrackets)
+                lines.Add("{");
+        }
+
+
+        public async Task WriteFile() => await File.WriteAllLinesAsync(_path, lines);
+    }
+}
